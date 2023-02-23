17adc5dfd80160e0c013d8b328ca5a1f2e7b851e
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-02-23 10:26:03 +0100

    zsh with prezto

--- homemanager/maltepc.nix
+++ homemanager/maltepc.nix
@@ -22,4 +22,27 @@
     source = ./config;
     recursive = true;
   };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true; # not finished if system package completion is wanted (look at home manager documentation)
+    autocd = true;
+    defaultKeymap = "vicmd";
+    history.save = 1000;
+    history.size = 1000;
+    initExtra = "setopt extended_glob\nbindkey 'kj' vi-cmd-mode";
+    shellAliases = {
+      "bat" = "bat --theme gruvbox-dark";
+      "tree" = "tree -C";
+      "tt" = "taskwarrior-tui";
+      "cp" = "cp --reflink=auto";
+    };
+    prezto = {
+      enable = true;
+      editor = {
+        dotExpansion = true;
+        keymap = "vi";
+      };
+    };
+  };
 }
