9cf81c98706a5cea90164f379b83c25e475588e1
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-02-23 10:51:24 +0100

    add sway-run

--- graphical/greetd.nix
+++ graphical/greetd.nix
@@ -9,11 +9,13 @@ let
       -b 'Poweroff' 'systemctl poweroff' \
       -b 'Reboot' 'systemctl reboot'
   '';
+  sway-run = pkgs.writeShellScriptBin "sway-run" (builtins.readFile ./sway-run.sh);
 in {
   environment.systemPackages = [
     pkgs.greetd.greetd
     pkgs.greetd.gtkgreet
     pkgs.sway
+    sway-run
   ]; 
   security.polkit.enable = true; # needed for seat management; couldn't get seatd to work
   services.greetd = {
