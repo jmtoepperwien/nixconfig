fccdd86f6795b0d37e022d6d6f5203d50c211925
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-02-24 13:05:11 +0100

    unfinished acme cert setup

--- programs/nginx.nix
+++ programs/nginx.nix
@@ -1,15 +1,31 @@
 { config, lib, pkgs, ... }:
 
 {
+  security.acme.acceptTerms = true;
+  security.acme.defaults.email = "m.toepperwien@protonmail.com";
+  security.acme.certs."mosihome.duckdns.org".extraDomainNames = [ "git.mosihome.duckdns.org" ];
   services.nginx = {
     enable = true;
-    virtualHosts."git.mosihome.duckdns.org" = {
-      forceSSL = true;
-      sslCertificate = "..."; # [TODO]
-      sslCertificateKey = "..."; # [TODO]
-      enableACME = true;
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:3000";
+    virtualHosts = {
+      "mosihome.duckdns.org" = { # [TODO] nextcloud
+        forceSSL = true;
+        enableACME = true;
+        sslCertificate = "..."; # [TODO]
+        sslCertificateKey = "..."; # [TODO]
+        enableACME = true;
+        locations."/" = {
+          root = "/var/www";
+        };
+      };
+      "git.mosihome.duckdns.org" = {
+        forceSSL = true;
+        enableACME = true;
+        useACMEHost = "mosihome.duckdns.org";
+        sslCertificate = "..."; # [TODO]
+        sslCertificateKey = "..."; # [TODO]
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:3000";
+        };
       };
     };
   };
