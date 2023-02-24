d9f12315bbe9e8e57b5e027b6e4f382260e197cf
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-02-24 13:07:29 +0100

    small changes

--- programs/nginx.nix
+++ programs/nginx.nix
@@ -10,19 +10,15 @@
       "mosihome.duckdns.org" = { # [TODO] nextcloud
         forceSSL = true;
         enableACME = true;
-        sslCertificate = "..."; # [TODO]
-        sslCertificateKey = "..."; # [TODO]
         enableACME = true;
         locations."/" = {
-          root = "/var/www";
+          root = "/var/www/mosihome";
         };
       };
       "git.mosihome.duckdns.org" = {
         forceSSL = true;
         enableACME = true;
         useACMEHost = "mosihome.duckdns.org";
-        sslCertificate = "..."; # [TODO]
-        sslCertificateKey = "..."; # [TODO]
         locations."/" = {
           proxyPass = "http://127.0.0.1:3000";
         };
