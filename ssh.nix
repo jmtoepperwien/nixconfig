074fd80e557815ccace8f76baa91c8127dec2421
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-03-26 23:37:45 +0200

    openssh high performance networking patches

--- ssh.nix
+++ ssh.nix
@@ -1,13 +1,12 @@
 { config, pkgs, lib, modulesPath, inputs, ... }:
 
 {
+  programs.ssh.package = pkgs.openssh_hpn;
   services.openssh = {
     enable = true;
+    openFirewall = true;
     settings.PasswordAuthentication = lib.mkForce false;
     settings.KbdInteractiveAuthentication = lib.mkForce false;
     settings.PermitRootLogin = lib.mkForce "no"; # nixos-generators will try to put this to true for first install
-    extraConfig = ''
-      UsePam no
-    '';
   };
 }
