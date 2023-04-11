697558c249a6e10918163fbcedf3f0425e565101
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-04-11 18:57:21 +0200

    add laptop ssh keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,13 +1,15 @@
 let
-  maltepchost = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII2uFpFcaacIH9yLyyABE0u2K1zuV/fph7RDA76zZ0wJ";
   maltepc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMIj8ipXRTEQYj+uPRlrSiJnybsC4lguYAA3KSS2/c3i";
+  maltexps = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGOBi7azxnZ0RhYXaIoE/axNkfuxnbaJ8Gs3CvLc3OT6 mtoepperwien@maltexps";
   users = [ maltepc ];
 
+  maltepchost = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII2uFpFcaacIH9yLyyABE0u2K1zuV/fph7RDA76zZ0wJ";
+  maltexpshost = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ92znHGW5UwpOfOJD/fejUKrsQLpSwSh4dt1xtIrLLs";
   pi3host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGTx5o298UgZ3gIBzFWwE+eOW3ACy0gXtdx71fcLdNvS";
   pi4host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICGDf5ouwaiRlKetP7zb1OxmeronwE2/8S+YwS7tY0g6";
   systems = [ maltepchost pi3host pi4host ];
 in {
-  "wifipassword.age".publicKeys = [ maltepc maltepchost pi3host pi4host ];
+  "wifipassword.age".publicKeys = [ maltepc maltepchost maltexps maltexpshost pi3host pi4host ];
   "nextcloud-adminpass.age".publicKeys = [ maltepc pi4host ];
-  "protonvpn.age".publicKeys = [ maltepc pi4host ];
+  "protonvpn.age".publicKeys = [ maltepc maltexps pi4host ];
 }
