1715d03f7a1b7c6727ff5f3688352afac4691799
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-04-11 19:02:36 +0200

    add flood rtorrent frontend

--- programs/torrent.nix
+++ programs/torrent.nix
@@ -25,4 +25,17 @@
       NetworkNamespacePath = "/var/run/netns/vpn";
     };
   };
+
+  systemd.services.flood = {
+    description = "Flood frontend for rtorrent";
+    bindsTo = [ "rtorrent.service" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.flood}/bin/flood --rtsocket /run/rtorrent/rpc.sock --port 5678";
+      Restart = "on-failure";
+      Type = "simple";
+      User = "rtorrent";
+      Group = "rtorrent";
+    };
+  };
+  networking.firewall.allowedTCPPorts = [ 5678 ];
 }
