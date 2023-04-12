036746bef9818574a76d7ae8792e0697ad0caf4e
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-04-12 16:48:55 +0200

    add protonvpn natpmp service

--- programs/torrent.nix
+++ programs/torrent.nix
@@ -17,6 +17,21 @@
     downloadDir = "/mnt/kodi_lib/downloads_torrent";
   };
 
+  systemd.services."natpmp-proton" = {
+    enable = true;
+    user = "root";
+    description = "Acquire incoming port from protonvpn natpmp";
+    requires = [ "protonvpn.service" ];
+    bindsTo = [ "protonvpn.service" ];
+    serviceConfig = {
+      NetworkNamespacePath = "/var/run/netns/vpn";
+      # [TODO: not hardcoded gateway]
+      ExecStartPre = "${pkgs.libnatpmp}/bin/natpmpc -g 10.2.0.1 -a 0 0 udp 60 | grep 'Mapped public port' | sed -E 's/.*Mapped public port ([0-9]+) .*/\1/' > /run/proton_udp_incoming && chown rtorrent:rtorrent /run/proton_udp_incoming";
+      ExecStart = "while true ; do ${pkgs.libnatpmp}/bin/natpmpc -g 10.2.0.1 -a 0 0 udp 60 && ${pkgs.libnatpmp}/bin/natpmpc -g 10.2.0.1 -a 0 0 tcp 60 || { echo -e \"ERROR with ${pkgs.libnatpmp}/bin/natpmpc command \a\" ; break ; } ; sleep 45 ; done";
+      Restart = "yes";
+    };
+  };
+
   systemd.services.rtorrent = {
     bindsTo = [ "netns@vpn.service" ];
     requires = [ "network-online.target" ];
@@ -29,7 +44,7 @@
   systemd.services.flood = {
     enable = true;
     description = "Flood frontend for rtorrent";
-    bindsTo = [ "rtorrent.service" ];
+    bindsTo = [ "rtorrent.service" "natpmp-proton.service" ];
     serviceConfig = {
       ExecStart = "${pkgs.flood}/bin/flood --rtsocket /run/rtorrent/rpc.sock --port 5678 --host 0.0.0.0";
       Restart = "on-failure";
