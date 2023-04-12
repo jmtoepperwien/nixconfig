5f42897ef91d62a4665a050d04853174b33b6d17
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-04-12 09:54:15 +0200

    fix gnome-wayland script

--- graphical/environments-maltexps.nix
+++ graphical/environments-maltexps.nix
@@ -4,7 +4,7 @@ let
   sway-run = pkgs.writeShellScriptBin "sway-run" (builtins.readFile ./sway-run.sh);
   gnome-wayland = pkgs.writeShellScriptBin "gnome-wayland" (builtins.readFile ./gnome-wayland.sh);
 in {
-  environment.systemPackages = [ gnome-wayland sway-run ];
+  environment.systemPackages = [ gnome-wayland sway-run pkgs.gnome.gnome-session ];
 
   environment.etc."greetd/environments".text = ''
     sway-run
