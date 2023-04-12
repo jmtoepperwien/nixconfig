1cfd4b2f063b31090b1e4cf252923058e0015019
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-04-12 10:28:26 +0200

    rtorrent tmux

--- programs/torrent.nix
+++ programs/torrent.nix
@@ -17,14 +17,18 @@
     downloadDir = "/mnt/kodi_lib/downloads_torrent";
   };
 
-  systemd.services.rtorrent = {
-    bindsTo = [ "netns@vpn.service" ];
-    requires = [ "network-online.target" ];
-    after = [ "protonvpn.service" ];
-    serviceConfig = {
-      NetworkNamespacePath = "/var/run/netns/vpn";
+  systemd.services.rtorrent = let
+      configFile = pkgs.writeText "rtorrent.rc" config.services.rtorrent.configText;
+    in {
+      bindsTo = [ "netns@vpn.service" ];
+      requires = [ "network-online.target" ];
+      after = [ "protonvpn.service" ];
+      serviceConfig = {
+        NetworkNamespacePath = "/var/run/netns/vpn";
+        ExecStart = "${pkgs.tmux}/bin/tmux -L rt new-session -s rt -n rtorrent -d rtorrent ${pkgs.rtorrent}/bin/rtorrent -n -o import=${configFile}";
+        ExecStop = "${pkgs.bash}/bin/bash -c \"${pkgs.tmux}/bin/tmux -L rt send-keys -t rt:rtorrent.0 C-q; while pidof rtorrent > /dev/null; do echo stopping rtorrent...; sleep 1; done\"";
+      };
     };
-  };
 
   systemd.services.flood = {
     enable = true;
