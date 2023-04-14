e37652e2498f9160ecf3237f3656221a2fda7aaf
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-04-14 15:17:32 +0200

    add man pages for libc etc

--- common/devel.nix
+++ common/devel.nix
@@ -1,21 +1,25 @@
 { config, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = [
-    pkgs.gnumake
-    pkgs.cmake
-    pkgs.git
-    pkgs.gnused
-    pkgs.xz
-    pkgs.zip
-    pkgs.gnutar
-    pkgs.llvmPackages.libunwind
-    pkgs.llvmPackages.libcxxStdenv
-    pkgs.llvmPackages.libcxxClang
-    pkgs.libcxx
-    pkgs.gcc
-    pkgs.lua
-    pkgs.lua51Packages.lua
-    pkgs.luajit
+  environment.systemPackages = with pkgs; [
+    # man pages for linux api not there by default
+    man-pages
+    man-pages-posix
+    gnumake
+    cmake
+    git
+    gnused
+    xz
+    zip
+    gnutar
+    llvmPackages.libunwind
+    llvmPackages.libcxxStdenv
+    llvmPackages.libcxxClang
+    libcxx
+    gcc
+    lua
+    lua51Packages.lua
+    luajit
   ];
+  documentation.dev.enable = true;
 }
