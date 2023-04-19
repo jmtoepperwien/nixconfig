9efa527c8064c1c3bef19fec3a51851d8c62f921
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-04-19 10:40:05 +0200

    do not confirm completions with enter

--- homemanager/config/nvim/lua/lsp-cmp-setup.lua
+++ homemanager/config/nvim/lua/lsp-cmp-setup.lua
@@ -20,7 +20,7 @@ cmp.setup({
 		['<C-j>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		-- ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 		['<Tab>'] = function(fallback) if cmp.visible() then cmp.select_next_item() else fallback() end end,
 	}),
 	sources = cmp.config.sources({
