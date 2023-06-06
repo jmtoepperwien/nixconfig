07ee1de5ff3b3a0a7499cc5a92ca9b78fc8600e3
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-06-06 16:56:14 +0200

    gnome shell

--- graphical/environments-maltexps.nix
+++ graphical/environments-maltexps.nix
@@ -4,7 +4,7 @@ let
   sway-run = pkgs.writeShellScriptBin "sway-run" (builtins.readFile ./sway-run.sh);
   gnome-wayland = pkgs.writeShellScriptBin "gnome-wayland" (builtins.readFile ./gnome-wayland.sh);
 in {
-  environment.systemPackages = [ gnome-wayland sway-run pkgs.gnome.gnome-session ];
+  environment.systemPackages = [ gnome-wayland sway-run pkgs.gnome.gnome-session  pkgs.gnome.gnome-shell ];
 
   environment.etc."greetd/environments".text = ''
     sway-run
