c7e9c3edd90bf231c63d5cb60dea887bfc69a387
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-06-14 10:13:55 +0200

    add books to nfs pi4

--- programs/kodi_nfs.nix
+++ programs/kodi_nfs.nix
@@ -9,13 +9,19 @@
     device = "/mnt/kodi_lib/series";
     options = [ "bind" ];
   };
+  fileSystems."/export/books" = {
+    device = "/mnt/kodi_lib/books";
+    options = [ "bind" ];
+  };
 
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
     /export/series  192.168.1.244(rw,async,insecure,subtree_check,all_squash,anongid=988)
     /export/movies  192.168.1.244(rw,async,insecure,subtree_check,all_squash,anongid=988)
+    /export/books  192.168.1.244(rw,async,insecure,subtree_check,all_squash,anongid=988)
     /export/series  192.168.1.149(rw,async,insecure,subtree_check,all_squash,anongid=988)
     /export/movies  192.168.1.149(rw,async,insecure,subtree_check,all_squash,anongid=988)
+    /export/books  192.168.1.149(rw,async,insecure,subtree_check,all_squash,anongid=988)
   '';
 
   networking.firewall.allowedTCPPorts = [ 2049 ];
