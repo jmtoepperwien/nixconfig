caebd3272fae7b6797b601ce1aece10eac3c4769
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-06-18 10:37:14 +0200

    fix typo

--- homemanager/config/nvim/lua/plugins.lua
+++ homemanager/config/nvim/lua/plugins.lua
@@ -235,7 +235,7 @@ return require("lazy").setup({
   { 'Aasim-A/scrollEOF.nvim',         config = function() require('scrollEOF').setup() end },
   { 'tpope/vim-eunuch' },
 
-  { 'figsoda/nix-developp.nvim' },
+  { 'figsoda/nix-develop.nvim' },
 
   --{ "Exafunction/codeium.vim" }
 
