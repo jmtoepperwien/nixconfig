1c60e76b6eb70aec04832dd900ea8a36ec9e8f26
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-07-15 17:14:43 +0200

    replace irssi with weechat

--- programs/irssi.nix
+++ programs/irssi.nix
@@ -1,22 +1,22 @@
 { config, lib, pkgs, ... }:
 
 {
-  nixpkgs.overlays = [ (final: prev: {
-    irssi =  prev.irssi.overrideAttrs ( oldAttrs: rec {
-      buildInputs = with pkgs; [
-        glib
-        libgcrypt
-        libintl
-        libotr
-        ncurses
-        openssl
-        (perl.withPackages ( ps: with ps; [ libwwwperl ] ))
-      ];
-      } );
-  })];
-  systemd.services."irssi" = {
+  #nixpkgs.overlays = [ (final: prev: {
+  #  irssi =  prev.irssi.overrideAttrs ( oldAttrs: rec {
+  #    buildInputs = with pkgs; [
+  #      glib
+  #      libgcrypt
+  #      libintl
+  #      libotr
+  #      ncurses
+  #      openssl
+  #      (perl.withPackages ( ps: with ps; [ libwwwperl ] ))
+  #    ];
+  #    } );
+  #})];
+  systemd.services."weechat" = {
     enable = true;
-    description = "irssi irc client inside tmux";
+    description = "weechat irc client inside tmux";
     wants = [ "network-online.target" ];
     after = [ "network-online.target" ];
     wantedBy = [ "default.target" ];
@@ -26,13 +26,8 @@
       User = "pi4";
       TimeoutSec = "5";
       Environment = "WIDTH=80 HEIGHT=24";
-      ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.tmux}/bin/tmux -L %p -2 new-session -A -d -x $WIDTH -y $HEIGHT -s irssi ${pkgs.irssi}/bin/irssi'";
-      ExecStop = ''
-        ${pkgs.tmux}/bin/tmux -L %p send-keys -t irssi:0 C-u
-        ${pkgs.tmux}/bin/tmux -L %p send-keys -t irssi:0 \quit\" Enter"
-        ${pkgs.bash}/bin/bash -c '${pkgs.toybox}/bin/timeout 3 ${pkgs.toybox}/bin/tail --pid=$(${pkgs.toybox}/bin/cat %t/irssi.pid || ${pkgs.toybox}/bin/echo 0) -f /dev/null'
-        -${pkgs.tmux}/bin/tmux -L %p kill-session -t irssi
-      '';
+      ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.tmux}/bin/tmux -L weechat new -d -s weechat ${pkgs.weechat}/bin/weechat'";
+      ExecStop = "${pkgs.tmux}/bin/tmux -L weechat kill-session -t weechat";
     };
   };
 }
