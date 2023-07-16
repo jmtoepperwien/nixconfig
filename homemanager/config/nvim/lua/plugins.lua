e025654381390ad808024df7f14b1e50dbeed4fc
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-07-16 16:26:33 +0200

    add nvim which-key plugin

--- homemanager/config/nvim/lua/plugins.lua
+++ homemanager/config/nvim/lua/plugins.lua
@@ -236,6 +236,19 @@ return require("lazy").setup({
 
   { 'figsoda/nix-develop.nvim' },
 
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
   --{ "Exafunction/codeium.vim" }
 
 })
