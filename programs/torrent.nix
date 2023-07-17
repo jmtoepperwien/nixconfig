6fd10a9dc9bc626d4e13f0e786d7bc7575354a35
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-07-17 14:44:04 +0200

    switch to transmission

--- programs/torrent.nix
+++ programs/torrent.nix
@@ -4,8 +4,8 @@ let
   libtorrent = pkgs.callPackage ./libtorrent.nix {};
   rtorrent = pkgs.callPackage ./rtorrent.nix { libtorrent = libtorrent; };
 in {
-  users.groups."rtorrent" = {};
-  users.users."rtorrent" = {
+  users.groups."transmission" = {};
+  users.users."transmission" = {
     isSystemUser = true;
     group = "usenet";
     extraGroups= [ "rtorrent" ];
@@ -13,22 +13,33 @@ in {
 
   environment.systemPackages = [ pkgs.flood pkgs.unpackerr ];
 
-  services.rtorrent = {
+  services.transmission = {
     enable = true;
-    package = pkgs.jesec-rtorrent;
-    user = "rtorrent";
+    package = pkgs.transmission_4;
+    user = "transmission";
     group = "usenet";
-    dataPermissions = "0775";
-    downloadDir = "/mnt/kodi_lib/downloads_torrent";
-    configText = ''
-      dht.mode.set = auto
-      protocol.pex.set = yes
-
-      trackers.use_udp.set = yes
-
-      system.umask.set = 0002
-    '';
+    settings.umask = 2;
+    settings.rpc-bind-address = "unix:/run/transmission.sock";
+    settings.download-dir = "/mnt/kodi_lib/tmp";
+    openRPCPort = true;
   };
+
+#  services.rtorrent = {
+#    enable = true;
+#    package = pkgs.jesec-rtorrent;
+#    user = "rtorrent";
+#    group = "usenet";
+#    dataPermissions = "0775";
+#    downloadDir = "/mnt/kodi_lib/downloads_torrent";
+#    configText = ''
+#      dht.mode.set = auto
+#      protocol.pex.set = yes
+#
+#      trackers.use_udp.set = yes
+#
+#      system.umask.set = 0002
+#    '';
+#  };
 
   systemd.services."natpmp-proton" = {
     enable = true;
@@ -82,10 +93,26 @@ in {
     };
   };
 
-  systemd.services.rtorrent = let
-    configFile = pkgs.writeText "rtorrent.rc" config.services.rtorrent.configText;
-    rtorrentPackage = config.services.rtorrent.package;
-  in {
+#  systemd.services.rtorrent = let
+#    configFile = pkgs.writeText "rtorrent.rc" config.services.rtorrent.configText;
+#    rtorrentPackage = config.services.rtorrent.package;
+#  in {
+#    bindsTo = [ "netns@vpn.service" ];
+#    requires = [ "network-online.target" "protonvpn.service" "natpmp-proton.service" "natpmp-forward.service" ];
+#    after = [ "protonvpn.service" "natpmp-proton.service" "natpmp-forward.service" ];
+#    wantedBy = [ "multi-user.target" ];
+#    serviceConfig = {
+#      EnvironmentFile = "/run/proton_incoming";
+#      NetworkNamespacePath = "/var/run/netns/vpn";
+#      ExecStart = lib.mkForce (pkgs.writers.writeBash "start-rtorrent" ''
+#        echo "${rtorrentPackage}/bin/rtorrent -n -o system.daemon.set=true -o import=${configFile} -o network.port_range.set=$TCPPORTPUBLIC-$TCPPORTPUBLIC -o dht.port.set=$UDPPORTPUBLIC
+#"
+#        ${rtorrentPackage}/bin/rtorrent -n -o system.daemon.set=true -o import=${configFile} -o network.port_range.set=$TCPPORTPUBLIC-$TCPPORTPUBLIC -o dht.port.set=$UDPPORTPUBLIC
+#      '');
+#    };
+#  };
+
+  systemd.services.transmission = {
     bindsTo = [ "netns@vpn.service" ];
     requires = [ "network-online.target" "protonvpn.service" "natpmp-proton.service" "natpmp-forward.service" ];
     after = [ "protonvpn.service" "natpmp-proton.service" "natpmp-forward.service" ];
@@ -93,26 +120,22 @@ in {
     serviceConfig = {
       EnvironmentFile = "/run/proton_incoming";
       NetworkNamespacePath = "/var/run/netns/vpn";
-      ExecStart = lib.mkForce (pkgs.writers.writeBash "start-rtorrent" ''
-        echo "${rtorrentPackage}/bin/rtorrent -n -o system.daemon.set=true -o import=${configFile} -o network.port_range.set=$TCPPORTPUBLIC-$TCPPORTPUBLIC -o dht.port.set=$UDPPORTPUBLIC
-"
-        ${rtorrentPackage}/bin/rtorrent -n -o system.daemon.set=true -o import=${configFile} -o network.port_range.set=$TCPPORTPUBLIC-$TCPPORTPUBLIC -o dht.port.set=$UDPPORTPUBLIC
-      '');
+      ExecStart = lib.mkForce "${config.services.transmission.package}/bin/transmission-daemon -f -g ${config.services.transmission.home}/.config/transmission-daemon ${lib.escapeShellArgs config.services.transmission.extraFlags} --peerport $TCPPORTPUBLIC --no-portmap";
     };
   };
 
   systemd.services.flood = {
     enable = true;
     description = "Flood frontend for rtorrent";
-    bindsTo = [ "rtorrent.service" ];
-    after = [ "rtorrent.service" ];
+    bindsTo = [ "transmission.service" ];
+    after = [ "transmission.service" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.flood}/bin/flood --rtsocket /run/rtorrent/rpc.sock --port 5678 --host 0.0.0.0";
+      ExecStart = "${pkgs.flood}/bin/flood --trurl unix:/run/transmission.sock --host 0.0.0.0";
       Restart = "on-failure";
       Type = "simple";
-      User = "rtorrent";
-      Group = "rtorrent";
+      User = "transmission";
+      Group = "transmission";
     };
   };
   networking.firewall.allowedTCPPorts = [ 5678 5656 ];
