3b927868aa5e21928c0606264b5cf9f375eea4ef
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-07-27 09:50:08 +0200

    downgrade from unstable to 23.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
-    home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-23.05"; };
+    home-manager = { url = "github:nix-community/home-manager/release-23.05"; inputs.nixpkgs.follows = "nixpkgs"; };
     agenix = { url = "github:ryantm/agenix"; };
     nixos-hardware = { url = "github:NixOS/nixos-hardware/master"; };
   };
