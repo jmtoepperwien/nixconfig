9a5119e6a2a04da4fe5fdb7d72cee317aed74ee3
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-09-11 15:11:35 +0200

    mergerfs with new drives

--- system/hardware/pi4.nix
+++ system/hardware/pi4.nix
@@ -18,14 +18,34 @@
       fsType = "ext4";
     };
 
-  fileSystems."/mnt/kodi_lib" =
+
+  systemd.tmpfiles.rules = [ "d /mnt/lib0 0775 pi4 usenet" "d /mnt/lib1 0775 pi4 usenet" "d /mnt/kodi_lib 0775 pi4 usenet" ];
+  fileSystems."/mnt/lib0" =
     { device = "/dev/disk/by-uuid/af71a65d-c25d-40c8-98fa-792c61de0630";
+      fsType = "btrfs";
+      options = [ "subvol=@" "defaults" ];
+    };
+  fileSystems."/mnt/lib1" =
+    { device = "/dev/disk/by-uuid/0dc95e30-d5bc-4385-b883-15ba5aaa172d";
       fsType = "btrfs";
       options = [ "subvol=@" "defaults" ];
     };
   services.btrfs.autoScrub = {
     enable = true;
     interval = "weekly";
+  };
+
+  systemd.services.mergerfskodilib = {
+    after = [ "mnt-lib0.mount" "mnt-lib1.mount" ];
+    requires = [ "mnt-lib0.mount" "mnt-lib1.mount" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig = {
+      Type = "simple";
+      KillMode = "none";
+      ExecStart = "${pkgs.mergerfs}/bin/mergerfs -o cache.files=partial,dropcacheonclose=true,category.create=mfs /mnt/lib0 /mnt/lib1 /mnt/kodi_lib";
+      ExecStop = "${pkgs.fuse}/bin/fusermount -uz /mnt/kodi_lib";
+      Restart = "on-failure";
+    };
   };
 
   swapDevices = [ ];
