c22c98f64f8c97c2adbd82951585f38a3e9b5fff
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-10-02 22:28:24 +0200

    navidrome

---
+++ programs/navidrome.nix
@@ -0,0 +1,12 @@
+{ config, lib, pkgs, agenix, ... }:
+{
+  systemd.tmpfiles.rules = [ "d /mnt/kodi_lib/music 0755 usenet usenet" ];
+  services.navidrome = {
+    enable = true;
+    settings = {
+      Address = "0.0.0.0";
+      Port = "3333";
+      MusicFolder = "/mnt/kodi_lib/music";
+    };
+  };
+}
