bf31454ad195ceffa12d6b36f52e4b0d461879c1
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-10-14 15:25:33 +0200

    add music to nfs exports

--- programs/kodi_nfs.nix
+++ programs/kodi_nfs.nix
@@ -13,15 +13,21 @@
     device = "/mnt/kodi_lib/books";
     options = [ "bind" ];
   };
+  fileSystems."/export/music" = {
+    device = "/mnt/kodi_lib/music";
+    options = [ "bind" ];
+  };
 
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
     /export/series  192.168.1.244(ro,async,insecure,all_squash,anongid=988,fsid=10)
     /export/movies  192.168.1.244(ro,async,insecure,all_squash,anongid=988,fsid=11)
     /export/books  192.168.1.244(ro,async,insecure,all_squash,anongid=988,fsid=12)
+    /export/music  192.168.1.244(ro,async,insecure,all_squash,anongid=988,fsid=12)
     /export/series  192.168.1.149(ro,async,insecure,all_squash,anongid=988,fsid=10)
     /export/movies  192.168.1.149(ro,async,insecure,all_squash,anongid=988,fsid=11)
     /export/books  192.168.1.149(ro,async,insecure,all_squash,anongid=988,fsid=12)
+    /export/music  192.168.1.149(ro,async,insecure,all_squash,anongid=988,fsid=12)
   '';
 
   networking.firewall.allowedTCPPorts = [ 2049 ];
