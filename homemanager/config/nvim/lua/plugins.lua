a2f1d04701e930a48584c9853448ecf452a22790
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-10-23 14:24:17 +0200

    little fix

--- homemanager/config/nvim/lua/plugins.lua
+++ homemanager/config/nvim/lua/plugins.lua
@@ -123,6 +123,7 @@ return require("lazy").setup({
     "HiPhish/rainbow-delimiters.nvim",
     dependencies = { "nvim-treesitter" },
     config = function()
+      local rainbow_delimiters = require("rainbow-delimiters")
       require("rainbow-delimiters.setup").setup({
         strategy = {
           [''] = rainbow_delimiters.strategy['global'],
