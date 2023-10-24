25f15d577cd7895f748c4b460a1848f8302be720
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2023-10-24 12:48:51 +0200

    music nfs right fsid

--- programs/kodi_nfs.nix
+++ programs/kodi_nfs.nix
@@ -23,11 +23,11 @@
     /export/series  192.168.1.244(ro,async,insecure,all_squash,anongid=988,fsid=10)
     /export/movies  192.168.1.244(ro,async,insecure,all_squash,anongid=988,fsid=11)
     /export/books  192.168.1.244(ro,async,insecure,all_squash,anongid=988,fsid=12)
-    /export/music  192.168.1.244(ro,async,insecure,all_squash,anongid=988,fsid=12)
+    /export/music  192.168.1.244(ro,async,insecure,all_squash,anongid=988,fsid=13)
     /export/series  192.168.1.149(ro,async,insecure,all_squash,anongid=988,fsid=10)
     /export/movies  192.168.1.149(ro,async,insecure,all_squash,anongid=988,fsid=11)
     /export/books  192.168.1.149(ro,async,insecure,all_squash,anongid=988,fsid=12)
-    /export/music  192.168.1.149(ro,async,insecure,all_squash,anongid=988,fsid=12)
+    /export/music  192.168.1.149(ro,async,insecure,all_squash,anongid=988,fsid=13)
   '';
 
   networking.firewall.allowedTCPPorts = [ 2049 ];
