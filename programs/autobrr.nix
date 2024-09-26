9995ad7420cbafa242041221d6f3c00f9daa859e
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2024-09-26 09:18:19 +0200

    update autobrr

--- programs/autobrr.nix
+++ programs/autobrr.nix
@@ -1,10 +1,10 @@
 { stdenv, lib, fetchzip, git, nodePackages, ... }:
 
 let
-  version = "1.29.0";
+  version = "1.46.1";
   src = fetchzip {
-    url = "https://github.com/autobrr/autobrr/releases/download/v1.29.0/autobrr_1.29.0_linux_arm64.tar.gz";
-    hash = "sha256-XeCnCH5ltjw/g3+Mrm8jlPC0PrI/uCFHbQ8jtSCvCiU=";
+    url = "https://github.com/autobrr/autobrr/releases/download/v${version}/autobrr_${version}_linux_arm64.tar.gz";
+    hash = "sha256-T3G471y5nn4xyZpgd6tTXzgmELaFFRE4rkJDbzigJfk=";
     stripRoot = false;
   };
 in stdenv.mkDerivation {
