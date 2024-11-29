a6f6fdd6dea9566496023398f0f9f61bb0bbf3e1
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2024-11-29 15:35:51 +0100

    disable hererocks

--- homemanager/config/nvim/lua/plugins.lua
+++ homemanager/config/nvim/lua/plugins.lua
@@ -416,6 +416,6 @@ return require("lazy").setup({
   },
 }, {
   rocks = {
-    hererocks = true,     -- recommended if you do not have global installation of Lua 5.1.
+    hererocks = false,     -- recommended if you do not have global installation of Lua 5.1.
   },
 })
