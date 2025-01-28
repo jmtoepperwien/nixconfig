72608f40006d6595ee7d225d10c94db4c7e5e93b
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-01-28 10:16:35 +0100

    rekey all secrets for new pi4 key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -6,7 +6,7 @@ let
   maltepchost = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII2uFpFcaacIH9yLyyABE0u2K1zuV/fph7RDA76zZ0wJ";
   maltexpshost = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ92znHGW5UwpOfOJD/fejUKrsQLpSwSh4dt1xtIrLLs";
   pi3host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGTx5o298UgZ3gIBzFWwE+eOW3ACy0gXtdx71fcLdNvS";
-  pi4host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILjw17ehQfBSpD6F1AQev8GOCzksHTuT/hdtCeQgBtfL";
+  pi4host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEbDhNXRckcZW2FoQmR6CQFNG6XANuLcijh2vc3i1C3O";
   systems = [ maltepchost pi3host pi4host ];
 in {
   "wifipassword.age".publicKeys = [ maltepc maltepchost maltexps maltexpshost pi3host pi4host ];
