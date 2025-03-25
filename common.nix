9b471f4a4132e16a5453f0c1c6b09673c127e64b
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-03-25 12:24:25 +0100

    add nix-community cachix

--- common.nix
+++ common.nix
@@ -15,6 +15,14 @@
       experimental-features = nix-command flakes
     '';
   };
+  # Use extra caches for packages
+  nix.settings.substituters = [
+    "https://nix-community.cachix.org"
+  ];
+  nix.settings.trusted-public-keys = [
+    # Compare to the key published at https://nix-community.org/cache
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
 
   imports = [
     ./common/devel.nix
