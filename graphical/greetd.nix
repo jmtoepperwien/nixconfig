662ca2602f1b85912d1742795318986993ba1277
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-05-15 07:17:17 +0200

    greetd problems fixed?

--- graphical/greetd.nix
+++ graphical/greetd.nix
@@ -7,16 +7,17 @@
 let
   sway-run = pkgs.writeShellScriptBin "sway-run" (builtins.readFile ./sway-run.sh);
   sway-greetd-config = pkgs.writeText "sway-greetd-config" ''
+    include /etc/sway/config.d/*
+    exec systemctl --user import-environment WAYLAND_DISPLAY DISPLAY XDG_CURRENT_DESKTOP SWAYSOCK I3SOCK XCURSOR_SIZE XCURSOR_THEME
+    exec dbus-update-activation-environment WAYLAND_DISPLAY DISPLAY XDG_CURRENT_DESKTOP SWAYSOCK I3SOCK XCURSOR_SIZE XCURSOR_THEME
+    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
+
     bindsym Mod4+shift+e exec swaynag \
     -t warning \
     -m 'What do you want to do' \
     -b 'Poweroff' 'systemctl poweroff' \
     -b 'Reboot' 'systemctl reboot'
 
-    include /etc/sway/config.d/*
-    exec systemctl --user import-environment WAYLAND_DISPLAY DISPLAY XDG_CURRENT_DESKTOP SWAYSOCK I3SOCK XCURSOR_SIZE XCURSOR_THEME
-    exec dbus-update-activation-environment WAYLAND_DISPLAY DISPLAY XDG_CURRENT_DESKTOP SWAYSOCK I3SOCK XCURSOR_SIZE XCURSOR_THEME
-    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
   '';
 in
 {
@@ -29,7 +30,7 @@ in
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.dbus}/bin/dbus-run-session -- ${pkgs.sway}/bin/sway --config ${sway-greetd-config}";
+        command = "${pkgs.sway}/bin/sway --config ${sway-greetd-config}";
       };
     };
   };
