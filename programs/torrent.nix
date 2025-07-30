144d776089c7a260a252c23919fd8c5f854289c1
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-07-30 13:16:07 +0200

    fix(security): revoke prowlarr api-key and use new one through secrets

--- programs/torrent.nix
+++ programs/torrent.nix
@@ -381,7 +381,6 @@ in
   };
   systemd.services.cross-seed =
     let
-      trackers = "http://127.0.0.1:9696/17/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/26/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/8/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/21/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/19/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/18/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/23/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/13/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/27/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/13/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/30/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/31/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/32/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/33/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/34/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/35/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/36/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/38/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/39/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/40/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/41/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/42/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/43/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/44/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/46/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/47/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/49/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535 http://127.0.0.1:9696/50/api?apikey=3e3fcaccc58e414ca7fe8b76c4da0535";
       search-cadence = "2w";
       rss-cadence = "30min";
       delay = "60";
@@ -401,7 +400,7 @@ in
         EnvironmentFile = "${config.age.secrets.cross-seed.path}";
         WorkingDirectory = "/var/lib/cross-seed";
         NetworkNamespacePath = "/var/run/netns/vpn";
-        ExecStart = "${pkgs.cross-seed}/bin/cross-seed daemon --torznab ${trackers} --exclude-older '720 days' --exclude-recent-search '90 days' --rss-cadence ${rss-cadence} --delay ${delay} --snatch-timeout ${snatch-timeout} --search-timeout ${search-timeout} --torrent-dir ${torrent-dir} --output-dir ${output-dir} --include-single-episodes --include-non-videos --action save --match-mode partial --verbose --fuzzy-size-threshold ${fuzzy-thr} --api-key $apikey";
+        ExecStart = "${pkgs.cross-seed}/bin/cross-seed daemon --torznab $trackers --exclude-older '720 days' --exclude-recent-search '90 days' --rss-cadence ${rss-cadence} --delay ${delay} --snatch-timeout ${snatch-timeout} --search-timeout ${search-timeout} --torrent-dir ${torrent-dir} --output-dir ${output-dir} --include-single-episodes --include-non-videos --action save --match-mode partial --verbose --fuzzy-size-threshold ${fuzzy-thr} --api-key $apikey";
         Restart = "always";
       };
     };
