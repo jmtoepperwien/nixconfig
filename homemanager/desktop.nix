fc6bef9f6f9f2044a72ee1468542708a478e247c
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-09-25 10:44:03 +0200

    feat(desktop): new cursor

--- homemanager/desktop.nix
+++ homemanager/desktop.nix
@@ -136,6 +136,27 @@ in
     NIXOS_OZONE_WL = "1";
   };
 
+  home.pointerCursor =
+    let
+      getFrom = url: hash: name: {
+          gtk.enable = true;
+          x11.enable = true;
+          name = name;
+          size = 48;
+          package =
+            pkgs.runCommand "moveUp" {} ''
+              mkdir -p $out/share/icons
+              ln -s ${pkgs.fetchzip {
+                url = url;
+                hash = hash;
+              }} $out/share/icons/${name}
+          '';
+        };
+    in
+      getFrom
+        "https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Ice.tar.xz"
+        "sha256-SG/NQd3K9DHNr9o4m49LJH+UC/a1eROUjrAQDSn3TAU="
+        "Bibata-Modern-Ice";
   programs.fuzzel = {
     enable = true;
     settings = {
